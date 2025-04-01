d64ea5a2118bb761794952d05cb6a554d8f74deb
Yanni Nadur <yanninadur@gmail.com>
2025-04-01 17:22:19 -0300

    fix: cors, docker-compose

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
     return [
         'paths' => ['api/*', 'sanctum/csrf-cookie'],
         'allowed_methods' => ['*'],
-        'allowed_origins' => ['http://localhost:3000'],
+        'allowed_origins' => ['*'],
         'allowed_origins_patterns' => [],
         'allowed_headers' => ['*'],
         'exposed_headers' => [],
